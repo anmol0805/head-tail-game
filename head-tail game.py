dd3c8816ac33d5d7e261bc059e42e0fbfe154102
Anmol <119474035+anmol0805@users.noreply.github.com>
2022-12-29 23:23:52 +0530

    Add files via upload

---
+++ head-tail game.py
@@ -0,0 +1,8 @@
+#Write the rest of your code below this line 👇
+import random
+random_int=random.randint(0,1)
+# print(random_int)
+if random_int==0:
+    print("Tails")
+else:
+    print("Heads")    
